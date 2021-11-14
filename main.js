c6edc39d8c4d4d582997ef6cf312ba340539d689
DevFrog92 <yongsu1481@gmail.com>
2021-11-14 18:20:02 +0900

    add: deivce detector

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
 (function init() {
   const titleElem = document.querySelector(".title")
   const pElem = document.querySelector(".user_agent")
+  const deviceElem = document.querySelector(".device")
   const userAgentObject = navigator.userAgent
+  let isMobile = false
+  let device = 'web'
 
   pElem.innerHTML = `${userAgentObject}`
+
+  if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(userAgentObject)) {
+    isMobile = true
+    device = "tablet"
+  }
+  else if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(userAgentObject)) {
+    isMobile = true
+    device = "mobile"
+  }
+
+  if (isMobile) {
+    alert(`This device is ${device}`)
+  }
+
+  deviceElem.innerHTML = `${device}`
 
   if (userAgentObject.indexOf('OPR') > 0) {
     titleElem.innerHTML = 'This web is Opera!'
@@ -12,6 +30,11 @@
 
   if (userAgentObject.indexOf('Edg') > 0) {
     titleElem.innerHTML = 'This web is Edge!'
+    return
+  }
+
+  if (userAgentObject.indexOf('Firefox') > 0) {
+    titleElem.innerHTML = 'This web is Firefox!'
     return
   }
 
