f82e1c333730c542d4c48d0a2c8c97e911eb2b28
DevFrog92 <yongsu1481@gmail.com>
2021-11-14 22:28:00 +0900

    add: Whale browser detect

--- main.js
+++ main.js
@@ -4,12 +4,11 @@
   const deviceElem = document.querySelector(".device")
   const deviceElemForIE = document.querySelector(".device_IE")
   const userAgentObject = navigator.userAgent
-  const ua = window.navigator.userAgent
   let isMobile = false
   let device = 'web'
+  let allowedBrowserState = false
 
   pElem.innerHTML = "" + userAgentObject
-  deviceElemForIE.innerHTML = "" + ua
 
   if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(userAgentObject)) {
     isMobile = true
@@ -27,27 +26,43 @@
   deviceElem.innerHTML = "" + device
 
   if (userAgentObject.indexOf('OPR') > 0) {
+    allowedBrowserState = false
     titleElem.innerHTML = 'This web is Opera!'
     return
   }
 
   if (userAgentObject.indexOf('Edg') > 0) {
+    allowedBrowserState = false
     titleElem.innerHTML = 'This web is Edge!'
     return
   }
 
   if (userAgentObject.indexOf('Firefox') > 0) {
+    allowedBrowserState = false
     titleElem.innerHTML = 'This web is Firefox!'
     return
   }
 
+  if (userAgentObject.indexOf('Whale') > 0) {
+    allowedBrowserState = false
+    titleElem.innerHTML = 'This web is Whale!'
+    return
+  }
+
   if (userAgentObject.indexOf('Chrome') > 0 && userAgentObject.indexOf('Safari') > 0) {
+    allowedBrowserState = false
     titleElem.innerHTML = 'This web is Chrome!'
     return
   }
 
   if (userAgentObject.indexOf('Chrome') < 0 && userAgentObject.indexOf('Safari') > 0) {
+    allowedBrowserState = false
     titleElem.innerHTML = 'This web is Safari!'
     return
+  }
+
+  if (allowedBrowserState) {
+    alert("현재 브라우저는 지원하지 않습니다. Chrome, Edge, FireFox, Opera 및 Whale 브라우저는 이용해주세요. 감사합니다.")
+    window.close()
   }
 })()
